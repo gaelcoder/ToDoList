7cc3b011695e13fa141c694f1aab6587b5158594
Gabriel <minifinikogama@gmail.com>
2025-08-07 08:43:46 -0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { WelcomeLandingComponent } from './components/welcome-landing/welcome-landing.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [WelcomeLandingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'ToDoList';
 }
---
+++ src/app/components/welcome-landing/welcome-landing.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-landing',
+  imports: [],
+  templateUrl: './welcome-landing.component.html',
+  styleUrl: './welcome-landing.component.css'
+})
+export class WelcomeLandingComponent {
+
+}
