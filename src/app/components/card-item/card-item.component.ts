121648257c7cd8479960a6bb674ada721c1c2cd3
Gabriel <minifinikogama@gmail.com>
2025-08-07 22:28:57 -0300

    Landing section criada

---
+++ src/app/components/card-item/card-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-item',
+  imports: [],
+  templateUrl: './card-item.component.html',
+  styleUrl: './card-item.component.css'
+})
+export class CardItemComponent {
+
+}
