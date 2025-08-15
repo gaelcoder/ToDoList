b8e52b7214fcd22c2c809946edaa109d06cfe5f1
Gabriel <minifinikogama@gmail.com>
2025-08-15 09:11:32 -0300

    início do form section, criação do model de tarefas

---
+++ src/app/components/cards-section/cards-section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cards-section',
+  imports: [],
+  templateUrl: './cards-section.component.html',
+  styleUrl: './cards-section.component.css'
+})
+export class CardsSectionComponent {
+
+}
